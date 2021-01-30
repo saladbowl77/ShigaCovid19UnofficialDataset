4587c99586e28b9b72e9c2bf0129e152b8d5a7e2
saladbowl77 <sarakun.info@gmail.com>
2021-01-31 00:27:46 +0900

    updateData

--- git.sh
+++ git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-cd /home/pi/Desktop/python/ShigaCovid19UnofficialDataset; git pull origin main; git add .; git commit -m "updateData"; git push origin HEAD:main
+cd /home/pi/Desktop/python/ShigaCovid19UnofficialDataset; git pull origin master; git add .; git commit -m "updateData"; git push origin HEAD:master
