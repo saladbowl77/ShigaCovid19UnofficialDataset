53b0908ad51ee99bfe8a29c77b0b31618feec341
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2021-01-31 01:12:38 +0900

    updateData

--- rss.py
+++ rss.py
@@ -1,6 +1,7 @@
 import json #標準ライブラリ
 import csv #標準ライブラリ
 from os.path import join, dirname #標準ライブラリ
+from datetime import datetime
 
 import feedparser #pip install feedparser
 import requests #pip install request
@@ -31,47 +32,55 @@ with open(JsonPath) as f:
 
 rssArr = ["pref","pref_news","otsuE","otsuI","kusatu"]
 
-def tweetNewRss(rss):
-    print(rss.title)
-    print(rss.link)
-    # 好きな言葉をツイート
-    api.update_status(rss.title + '\n' + rss.link)
+def tweetNewRss(rss,type):
+    if(type == 'published'):
+        if rss.published[-3:] == "GMT":
+            uploadDate = datetime.strptime(rss.published, '%a, %d %b %Y %H:%M:%S %Z')
+        else:
+            uploadDate = datetime.strptime(rss.published, '%a, %d %b %Y %H:%M:%S %z')
+    elif(type == 'date'):
+        uploadDate = datetime.strptime(rss.date, '%Y-%m-%dT%H:%M:%S+09:00')
+    else:
+        uploadDate = ""
+    
+    
+    #print(rss.title + '\n' + rss.link + '\n サイト更新日 : ' + uploadDate.strftime('%Y/%m/%d %H:%M:%S'))
+
+    api.update_status(rss.title + '\n' + rss.link + '\n サイト更新日 : ' + uploadDate.strftime('%Y/%m/%d %H:%M:%S'))
+    
 
 for name in rssArr:
-    print(jsonData[name])
-    print(jsonData[name]['url'])
-    print(jsonData[name]['last'])
-    print(jsonData[name]['word'])
+    feed = feedparser.parse(jsonData[name]['url'], response_headers={"content-type": "text/xml; charset=utf-8"})
 
     first = True
-    feed = feedparser.parse(jsonData[name]['url'], response_headers={"content-type": "text/xml; charset=utf-8"})
     for rss in feed.entries:
-        if rss.get('published') == None:
-            if rss.date > jsonData[name]['last']:
-                if jsonData[name]['word']:
-                    if jsonData[name]['word'] in rss.title:
-                        if first:
-                            jsonData[name]['last'] = rss.date
-                            first = False
-                        tweetNewRss(rss)
-                else:
-                    if first:
-                        jsonData[name]['last'] = rss.date
-                        first = False
-                    tweetNewRss(rss)
-        else:
+        if rss.get('published'):
             if rss.published > jsonData[name]['last']:
                 if jsonData[name]['word']:
-                    if jsonData[name]['word'] in rss.title:
-                        if first:
-                            jsonData[name]['last'] = rss.published
-                            first = False
-                        tweetNewRss(rss)
+                    if jsonData[name]['word'][:4] == "http" and jsonData[name]['word'] in rss.link:
+                        tweetNewRss(rss,'published')
+                    elif jsonData[name]['word'] in rss.title:
+                        tweetNewRss(rss,'published')
                 else:
-                    if first:
-                        jsonData[name]['last'] = rss.published
-                        first = False
-                    tweetNewRss(rss)
+                    tweetNewRss(rss,'published')
+            if first:
+                jsonData[name]['last'] = rss.published
+                first = False
+        else:
+            print(rss.link, rss.title)
+            if rss.date > jsonData[name]['last']:
+                if jsonData[name]['word']:
+                    if jsonData[name]['word'][:4] == "http" and jsonData[name]['word'] in rss.link:
+                        tweetNewRss(rss,'date')
+                    elif jsonData[name]['word'] in rss.title:
+                        tweetNewRss(rss,'date')
+                else:
+                    tweetNewRss(rss,'date')
+            if first:
+                jsonData[name]['last'] = rss.date
+                first = False
+
+print(jsonData)
 
 with open(JsonPath, 'w') as f:
     json.dump(jsonData, f, indent=4, ensure_ascii=False)
